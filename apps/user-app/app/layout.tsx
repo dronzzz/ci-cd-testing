0c67add8243fe1bede4a4743ad88b424baa0f5fd
dronzzz <mayank25rathod@gmail.com>
2025-01-13 16:34:38 +0530

    tessssssssssssst

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -23,6 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
+
+
+
+        
         <body className={inter.className}>
           <div className="min-w-screen min-h-screen bg-[#ebe6e6]">
             <AppbarClient />
