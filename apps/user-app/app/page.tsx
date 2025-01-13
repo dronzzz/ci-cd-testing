6c4749156bc43b04a8a65838f127d3a569a6d891
dronzzz <mayank25rathod@gmail.com>
2025-01-13 15:53:40 +0530

    testing workflow

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -6,6 +6,13 @@ export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect('/dashboard')
+
+
+
+
+
+
+    
   } else {
     redirect('/api/auth/signin')
   }
