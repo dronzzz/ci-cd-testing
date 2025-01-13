ad584464aa6ffe947837976336bf65d026368186
dronzzz <mayank25rathod@gmail.com>
2025-01-13 16:04:26 +0530

    testing 4

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -7,8 +7,8 @@ import { AppbarClient } from "../components/AppbarClient";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Wallet",
-  description: "Simple wallet designed for eaze of use",
+  title: "VWallet",
+  description: "Simple wallet designed for balzing fast transactions ",
 };
 
 export default function RootLayout({
